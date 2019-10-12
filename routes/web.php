e6f0da187717232b2fa4d62a883d80f83b98df07
naqibfaiyaz <naqib3110@gmail.com>
2019-10-12 22:55:25 +0600

    Fix controller name

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('merchantData', 'merchantDataVieWController');
+Route::resource('merchantData', 'merchantDataViewController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
